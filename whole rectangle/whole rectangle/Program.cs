86b2e28df32ac15f3a0aa6583edb3bd9d7596310
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 15:24:19 +0530

    Add files via upload

---
+++ whole rectangle/whole rectangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whole_rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n = 4, m = 5;
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    if (i == 1 || j == 1 || i == 4 || j == 5)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
