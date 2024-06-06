c078eb98842ef8f915911204b30f2aa2e6e80ec4
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:19:39 +0530

    Add files via upload

---
+++ 2 number/2 number/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int num3;
+            Console.WriteLine("enter num");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter num");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter num");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if(num1>num2)
+            {
+                if(num1>num3)
+                {
+                    Console.WriteLine("num1 is grater " + num1);
+                }
+                else
+                {
+                    Console.WriteLine("num3 is grater " + num3);
+                }
+            }
+            else if(num2>num3)
+            {
+                Console.WriteLine("num2 is grater " + num2);
+            }
+            else
+            {
+                Console.WriteLine("num3 is grater " + num3);
+            }
+        }
+    }
+}
