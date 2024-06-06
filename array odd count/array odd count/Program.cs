d63dd9fbe52e60c90ded9440487a3434611a96e6
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:24:07 +0530

    Add files via upload

---
+++ array odd count/array odd count/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_odd_count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count=0;
+            int[] n = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("enter no");
+                n[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (n[i]%2 != 0)
+                {
+                    count++;
+                }
+
+            }
+            Console.Write(count);
+
+        }
+    }
+}
