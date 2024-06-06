620732e37bf5f9e1fae8a0134af9a9575caa385e
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 15:01:12 +0530

    Add files via upload

---
+++ short syntax and dynamic initialisation ex/short syntax and dynamic initialisation ex/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace short_syntax_and_dynamic_initialisation_ex
+{
+    class product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string mfgname { get; set; }
+        public decimal price{ get; set; }
+        public void disProduct()
+        {
+            Console.WriteLine("product id :" + this.id);
+            Console.WriteLine("product name :" + this.name);
+            Console.WriteLine("product mfgname :" + this.mfgname );
+            Console.WriteLine("product price :" + this.price );
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            product p = new product { id = 1, name = "Processor", mfgname = "intel", price = 5000 };
+            p.disProduct();
+        }
+    }
+}
