e8cd296b6b98631bf460fcb53d1d826411826d89
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:33:28 +0530

    Add files via upload

---
+++ enum_ex1/enum_ex1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enum_ex1
+{
+    enum cities
+    {
+        pune=100,mumbai=200,delhi=300,noida=400
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int citycode;
+            double amount, discount_amount = 0;
+
+            Console.WriteLine("enter city code:");
+            citycode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter amount:");
+            amount = Convert.ToDouble(Console.ReadLine());
+
+            switch(citycode)
+            {
+                case (int)cities.pune:
+                    discount_amount = amount * ((double)10 / 100);
+                    break;
+                case (int)cities.mumbai:
+                    discount_amount = amount * ((double)20 / 100);
+                    break;
+                case (int)cities.delhi:
+                    discount_amount = amount * ((double)30 / 100);
+                    break;
+                case (int)cities.noida:
+                    discount_amount = amount * ((double)40 / 100);
+                    break;
+                default:
+                    Console.WriteLine("no discount...");
+                    break;
+            }
+            Console.WriteLine("discount amount:" + discount_amount);
+        }
+    }
+}
