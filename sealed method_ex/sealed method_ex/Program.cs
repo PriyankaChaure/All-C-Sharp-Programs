cd268a5048cb62c2e5e8d5380444863faceca7fc
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 15:00:26 +0530

    Add files via upload

---
+++ sealed method_ex/sealed method_ex/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sealed_method_ex
+{
+    class A
+    {
+        public virtual void create()
+        {
+            Console.WriteLine("A created");
+        }
+    }
+    class B:A
+    {
+        public sealed override void create()
+        {
+            Console.WriteLine("B created");
+        }
+    }
+    class C : B
+    {
+       // public  override void create()
+        //{
+          //  Console.WriteLine("C created");
+        //}
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //C c = new C();
+            //c.create();
+
+            B b = new B();
+                b.create();
+
+
+
+        }
+    }
+}
