64844f3dcae124a3c26a99c6dd3071c17cc93498
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:55:25 +0530

    Add files via upload

---
+++ pattern/Studentoop/Studentoop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studentoop
+{
+    internal class Program
+    {
+        internal class Student1
+        {
+            private int RollNo;
+            private string Name;
+            private string MobileNo;
+            private string CourseName;
+
+            public void AddStudent(int rn,string n,string mn,string cn)
+            {
+                RollNo = rn;
+                Name = n;
+                MobileNo = mn;
+                CourseName = cn;
+            }
+            public void DisplayStudent()
+            {
+                Console.WriteLine("RollNo:" + RollNo);
+                Console.WriteLine("name:" + Name);
+                Console.WriteLine("Mobile no:" + MobileNo);
+                Console.WriteLine("Course Name:" + CourseName);
+            }
+
+        }
+  
+        static void Main(string[] args)
+        {
+            Student1 s = new Student1();
+            s.AddStudent(1, "priya", "12345", "c#");
+            s.DisplayStudent();
+
+        }
+    }
+}
---
+++ pattern/pattern/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <=5 ; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+            for(int k=1; k<=4; k++)
+            {
+                for (int j = k; j <= 4; j++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
