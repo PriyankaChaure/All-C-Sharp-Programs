fd926ab1e58f4d58bbb84853f9c9ddd5989c7cfb
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 15:05:07 +0530

    Add files via upload

---
+++ static method/static method/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_method
+{
+    class person
+    {
+        public static int id;
+        public static string name;
+        public static int age;
+
+        public  void add(int i, string n, int a)
+        {
+            id = i;
+            name = n;
+            age = a;
+        }
+        public static void display()
+        {
+            person p = new person();
+            p.add(1, "priya", 23);
+         
+            Console.WriteLine("person id:" + id);
+            Console.WriteLine("person name:" + name);
+            Console.WriteLine("person age" + age);
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person.display();
+        }
+    }
+}
