5afcde56fa2975dfc9ef9d4189c24d1b0137007b
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:40:17 +0530

    Add files via upload

---
+++ extensionmethod for cntspace/extensionmethod for cntspace/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace extensionmethod_for_cntspace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "extension method for count space ";
+            Console.WriteLine("string is:" + s);
+            s.cntspace();
+            int r=s.cntspace();
+    
+            Console.WriteLine("count of space is:"+r);
+            s.cntchar();
+            int r1=s.cntchar();
+
+            Console.WriteLine("count of character is:" + r1);
+        }
+    }
+}
---
+++ extensionmethod for cntspace/extensionmethod for cntspace/space.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionmethod_for_cntspace
+{
+    public static class space
+    {
+        public static int cntspace(this string obj)
+        {
+            int count = 0;
+            for (int i = 0; i < obj.Length; i++)
+            {
+                
+                if (obj[i ]== ' ')
+                
+                    count++;
+                
+            }
+            return count;
+        }
+        //public static int cntchar(this string obj)
+        //{
+        //    int count = 0;
+        //    for (int i = 0; i < obj.Length; i++)
+        //    {
+        //        var v = obj.Replace(" ", "");
+        //        count++;
+        //    }
+        //    return count;
+        //}
+        public static int cntchar(this string obj)
+        {
+            int count = 0;
+            for (int i = 0; i < obj.Length; i++)
+            {
+
+                if (obj[i] != ' ')
+
+                    count++;
+
+            }
+            return count;
+        }
+
+
+    }
+}
