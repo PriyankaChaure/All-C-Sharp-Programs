4cf4ce108b6fa92dc4eb76539f3dc11cd34df56d
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:33:07 +0530

    Add files via upload

---
+++ enum_ex/enum_ex/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enum_ex
+{
+    
+        enum cities
+        {
+            pune,mumbai=200,noida, delhi=300,pcmc
+        }
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)cities.pune);
+            Console.WriteLine((int)cities.mumbai);
+            Console.WriteLine((int)cities.noida);
+            Console.WriteLine((int)cities.delhi);
+            Console.WriteLine((int)cities.pcmc);
+        }
+    }
+}
