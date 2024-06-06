4d0a37963fc5607ff9ffb13bd85a8a0d3fd17398
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:31:58 +0530

    Add files via upload

---
+++ display array/display array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace display_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] n = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("enter no");
+                n[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            for(int i=10-1; i>=0; i-- )
+           {
+                
+
+                Console.WriteLine(n[i]);
+                
+            }
+        }
+    }
+    
+}
