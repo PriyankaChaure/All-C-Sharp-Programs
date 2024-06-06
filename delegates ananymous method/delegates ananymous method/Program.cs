dbbdf3a9230329bd4d0268d70b01c05708ee763b
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:31:13 +0530

    Add files via upload

---
+++ delegates ananymous method/delegates ananymous method/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates_ananymous_method
+{
+    delegate void tdel();
+    delegate int adel(int b, int c);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            tdel t = delegate ()
+            {
+                Console.WriteLine("hello");
+            };
+            t();
+
+            adel a = delegate (int b, int c)
+            {
+                return b + c;
+            };
+            int r = a(4, 5);
+            Console.WriteLine("Addition is:" + r);
+
+        }
+    }
+}
