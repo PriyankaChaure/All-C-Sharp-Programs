cff0ef4458b0cc695e39c4922b07b545721588ec
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:24:31 +0530

    Add files via upload

---
+++ arrayexample/arrayexample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayexample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("enter no");
+                n[i] = Convert.ToInt32(Console.ReadLine());
+                
+            }
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine("series is"+n[i]);
+        }
+    }
+}
