f49358284f9d1ba978b64cbd7f8f066a169d16c7
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 15:07:20 +0530

    Add files via upload

---
+++ structure ex/structure ex/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structure_ex
+{
+    struct point
+    {
+        private int x;
+        private int y;
+
+        public void addpoint(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void dispoint()
+        {
+            Console.WriteLine("X:" + this.x);
+            Console.WriteLine("Y:" + this.y);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            point p = new point();
+            p.addpoint(23, 30);
+            p.dispoint();
+        }
+    }
+}
