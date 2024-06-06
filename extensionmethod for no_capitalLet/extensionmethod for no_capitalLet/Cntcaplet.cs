577ae087ff7eaa01d50e99d3ab9f6864c7e994d9
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:40:38 +0530

    Add files via upload

---
+++ extensionmethod for no_capitalLet/extensionmethod for no_capitalLet/Cntcaplet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionmethod_for_no_capitalLet
+{
+    public static class Cntcaplet
+    {
+        public static int cntletter(this string obj)
+        {
+            int count = 0;  
+            for (int i = 0;i<obj.Length;i++)
+            {
+                if (obj[i] >= 'A' && obj[i] <='Z')
+                    count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ extensionmethod for no_capitalLet/extensionmethod for no_capitalLet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionmethod_for_no_capitalLet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Prachi Priyanka";
+            int r = s.cntletter();
+            Console.WriteLine ("count of capital letters is:"+r);
+        }
+    }
+}
