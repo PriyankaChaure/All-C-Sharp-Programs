2ecf4a17ff8c42e88b71c780d90bb2fcf09dcc06
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:53:42 +0530

    Add files via upload

---
+++ operatoroverloading pmeg/operatoroverloading pmeg/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatoroverloading_pmeg
+{
+    class point
+    {
+        private int x;
+        private int y;
+        public void initpoint(int a,int b)
+        { 
+            this.x = a; 
+            this.y = b; 
+        }
+        public void dispoint()
+        {
+            Console.WriteLine("X:" + this.x);
+            Console.WriteLine("Y:" + this.y);
+
+        }
+        public static point operator++(point temp)
+        {
+            temp.x++;
+            temp.y++;
+            return temp;
+        }
+        public static point operator --(point temp)
+        {
+            temp.x--;
+            temp.y--;
+            return temp;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            point p = new point();
+            p.initpoint(3, 4);
+            p.dispoint();
+            p++;
+            p.dispoint();
+            p--;
+            p.dispoint();
+            p++;
+            p.dispoint();
+            p--;
+            p.dispoint();
+        }
+    }
+}
