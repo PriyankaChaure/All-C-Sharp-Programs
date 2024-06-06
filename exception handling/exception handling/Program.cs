86ff5be952f6e61195000124d244d934bfc3acea
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:34:50 +0530

    Add files via upload

---
+++ exception handling/exception handling/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a, b, c;
+                Console.WriteLine("enter 1st value:");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter 2nd value:");
+                b = Convert.ToInt32(Console.ReadLine());
+                c = a / b;
+                Console.WriteLine("division is:" + c);
+            }
+            catch (DivideByZeroException dv)
+            {
+                Console.WriteLine("divided by zero exception occured!!!");
+                Console.WriteLine("enter non-zero value...");
+                Console.WriteLine(dv.ToString());
+            }
+            catch(FormatException fe)
+            {
+                Console.WriteLine("format exception occured!!!");
+                Console.WriteLine("enter integer value");
+                Console.WriteLine(fe.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("final block executed!!!");
+            }
+
+        }
+    }
+}
