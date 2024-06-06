3e5402ea519d726d6b9c8464692a05d1a86db3a0
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 15:02:26 +0530

    Add files via upload

---
+++ star print/star print/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace star_print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=5;i>=1;i--)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
