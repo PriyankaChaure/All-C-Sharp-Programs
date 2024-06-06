860d8bad137f7b5ea39d03fccb29f42ba169ba98
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:56:28 +0530

    Add files via upload

---
+++ polycirrec/polycirrec/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polycirrec
+{  
+    class shapes
+    {
+        public void calculatearea(double radius)
+        {
+            Console.WriteLine("area of circle is:" + (3.14 * radius * radius));
+        }
+        public void calculatearea(int length, int width)
+        {
+            Console.WriteLine("area of rectangle is:" + (length*width));
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            shapes s = new shapes();
+            s.calculatearea(4.5);
+            s.calculatearea(3, 4);
+        }
+    }
+}
---
+++ polycirrec/polymorphism search/polymorphism search/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism_search
+{ internal class search
+    {
+
+      public bool dosearch(string[] str, string s)
+        {
+            bool flag = false;
+            foreach(string temp in str)
+            {
+                if(temp==s)
+                {
+                    flag = true;
+                }
+            }
+            return flag;
+        }
+
+        public bool dosearch(int[] num, int n)
+        {
+            bool flag = false;
+            foreach (int temp in num)
+            {
+                if (temp == n)
+                {
+                    flag = true;
+                }
+            }
+            return flag;
+        }
+
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            search se = new search();
+            string[] s = { "abc", "pqr", "xyz", "lmp" };
+            if(se.dosearch(s,"lmp"))
+            {
+                Console.WriteLine("String is found!!");
+            }
+            else
+            {
+                Console.WriteLine("String not found!!");
+            }
+
+            int[] num = { 123,456,789,147 };
+            if (se.dosearch(num, 147))
+            {
+                Console.WriteLine("Integer is found!!");
+            }
+            else
+            {
+                Console.WriteLine("Integer not found!!");
+            }
+
+        }
+    }
+}
