ba249dc42f52d6ccdb049366192802364dc5498c
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:41:30 +0530

    Add files via upload

---
+++ forloop addition/forloop addition/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forloop_addition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0;
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine("enter number");
+                n = Convert.ToInt32(Console.ReadLine());
+                //Console.WriteLine(n+"+");
+                sum = sum + n;
+            }
+            //Console.Write(sum + "+");
+            Console.WriteLine("addition is: " + sum);
+        }
+    }
+}
