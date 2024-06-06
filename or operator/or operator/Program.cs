1e4d626a68bf521cf286df2db8e9233976cd60cc
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:54:18 +0530

    Add files via upload

---
+++ or operator/or operator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace or_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int zinc, copper;
+            Console.WriteLine("enter zinc");
+            zinc= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter copper");
+            copper = Convert.ToInt32(Console.ReadLine());
+            if(zinc>80 ||copper>90)
+            {
+                Console.WriteLine("good quality");
+
+            }
+            else
+                Console.WriteLine("bad quality");
+
+
+        }
+    }
+}
