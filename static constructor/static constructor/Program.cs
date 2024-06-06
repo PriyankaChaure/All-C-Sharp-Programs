e0d99cc3d1a63ff5b4ac0824c835412bf6b0d5e9
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 15:03:27 +0530

    Add files via upload

---
+++ static constructor/static constructor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_constructor
+{
+    class counter
+    {
+        private static int count;
+        static counter()
+        {
+            count = 0;
+        }
+        public static void Increment()
+        {
+            count++;
+            Console.WriteLine("count is:" + count);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            counter.Increment();
+            counter.Increment();
+            counter.Increment(); 
+            counter.Increment();
+        }
+    }
+}
