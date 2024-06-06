313f0a485d67e038f2c2b749d7eeaa79a12b09cd
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 15:04:42 +0530

    Add files via upload

---
+++ static example/static example/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_example
+{
+    class sample
+    {
+        
+        public static int count=0;
+
+        public sample()
+        {
+            if (count >= 1)
+            {
+                Console.WriteLine("can not create more than one object class is singleton 2nd object called");
+            }
+            count++;
+
+        }
+       
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sample s = new sample();
+            sample s1 = new sample();
+            sample s2 = new sample();
+
+
+        }
+    }
+}
