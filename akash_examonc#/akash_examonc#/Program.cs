6a7213a5c306d0ddef7d3484e7f47ccd6b07bafa
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:22:04 +0530

    Add files via upload

---
+++ akash_examonc#/akash_examonc#/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akash_examonc_
+{
+    class  DataStore<T>
+    {
+        private string[] names = { "priyanka","prachi","sweety","guddi","sonali"};
+        public void addElement(string[] n)
+        {
+            this.names = n;
+        }
+        public void removeElement(T r)
+        {
+
+        }
+        public void displayAllElements()
+        {
+            Console.WriteLine(this.names);
+        }
+        public void insertElement(T i)
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            DataStore<string> d = new DataStore<string>();
+            d.displayAllElements();
+        }
+    }
+}
