d9b4f527f15b52ba0879bad2bac1145e9e654b0b
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 15:10:20 +0530

    Add files via upload

---
+++ vb/vb/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, dig, sum = 0, temp = 0, max=sum;
+            for (int i = 0; i < 10; i++)
+            {
+             //dig = i;
+               // sum += dig; 
+
+
+                Console.WriteLine("enter no:");
+
+                n = Convert.ToInt32(Console.ReadLine());
+
+                while (n != 0)
+                {
+                    dig = n % 10;
+                    sum = sum + dig;
+
+                    n = n / 10;
+
+                }
+                Console.WriteLine("addition:"+sum);
+                sum = temp;
+                
+            }
+
+        }
+    }
+}
