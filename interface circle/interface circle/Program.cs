d25a86a0f7adece8a9ae3aaf8bc2a5be3a8168b7
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:43:20 +0530

    Add files via upload

---
+++ interface circle/interface circle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+interface AreaInt
+{
+    void calculateArea(double r);
+    
+}
+class circle : AreaInt
+{
+    public void calculateArea(double r)
+    {
+        Console.WriteLine("Area of circle is:"+(3.14*r* r));
+    }
+}
+namespace interface_circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            circle c = new circle();
+            c.calculateArea(4.2);
+            
+        }
+    }
+}
