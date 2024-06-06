360acb998cf3f150178b17f30985c8b1b18d90f2
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:21:09 +0530

    Add files via upload

---
+++ addition/addition/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 4;
+            int c;
+            c = a + b;
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+        }
+    }
+}
