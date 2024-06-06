c482cf8701cd40ece39dd22eccd7d78260ceb7da
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:21:43 +0530

    Add files via upload

---
+++ addition digits max/addition digits max/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prachi_exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dig, sum=0, total;
+            int[] n = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("enter numbers:");
+                n[i] = Convert.ToInt32(Console.ReadLine());
+                while (n[i] != 0)
+                {
+                    dig = n[i] % 10;
+                    total = sum + dig;
+                    Console.WriteLine(total);
+                    n[i] = n[i] / 10;
+                }
+                
+
+
+
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
