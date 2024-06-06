f70596a0de6e2e61f49a9e2508917a7ca1a7a05d
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:43:45 +0530

    Add files via upload

---
+++ interface exam/interface exam/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+interface AreaInt
+{
+    void calculateArea(double r,double l);
+
+}
+class circle : AreaInt
+{
+    public void calculateArea(double r, double l)
+    {
+        Console.WriteLine("Area of circle is:" + (3.14 * r * r));
+    }
+}
+class rectangle : AreaInt
+{
+    public void calculateArea(double r, double l)
+    {
+        Console.WriteLine("Area of circle is:" + ( r * l));
+    }
+}
+namespace interface_circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            circle c = new circle();
+            c.calculateArea(4.2,0);
+            rectangle r = new rectangle();
+            r.calculateArea(3.2, 5.4);
+
+        }
+    }
+}
---
+++ interface exam/interface exam/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("interface exam")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("interface exam")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("46e459e6-8584-4382-8fd4-c0a66dbeabe9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
