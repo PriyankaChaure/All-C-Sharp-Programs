f1021c8bcc1eccc9a00433db1aa641e12ca89259
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:47:45 +0530

    Add files via upload

---
+++ matexp/matexp/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matexp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, dig;
+            Console.Write("enter no:");
+            n = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                dig = n % 10;
+                if (dig % 2 != 0)
+                {
+
+
+                    Console.Write("0");
+                }
+                else
+                    Console.Write(dig);
+                n = n / 10;
+            }
+            while (n != 0);
+           // Console.Write(n);
+        }
+    }
+}
