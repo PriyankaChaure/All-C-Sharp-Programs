b7b0f3adac96453d0da641e4027294f3b62f3ea9
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 15:02:03 +0530

    Add files via upload

---
+++ star pattern/star pattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace star_pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <=5; i++)
+            {
+                for (int j = i; j <= 5; j++)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
