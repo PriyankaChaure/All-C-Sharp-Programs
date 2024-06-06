600e7235f15c9a78e79642a20e94d0404dd2fcf0
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:34:04 +0530

    Add files via upload

---
+++ even odd count/even odd count/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace even_odd_count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, cnt1 = 0, cnt2 = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter no");
+                n = Convert.ToInt32(Console.ReadLine());
+                if(n%2==0)
+                {
+                    cnt1++;
+                }
+                else if(n%2!=0)
+                {
+                    cnt2++;
+                }
+            }
+            Console.WriteLine("even no count is:" + cnt1);
+            Console.WriteLine("odd no count is:" + cnt2);
+        }
+    }
+}
