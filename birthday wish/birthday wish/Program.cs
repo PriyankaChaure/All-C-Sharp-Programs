d311ccb28ddd6576071fca7dd7acb3060ebefbb4
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:26:51 +0530

    Add files via upload

---
+++ birthday wish/birthday wish/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace birthday_wish
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int month;
+            int date;
+            Console.WriteLine("enter birth month:");
+            month=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter birth date:");
+            date=Convert.ToInt32(Console.ReadLine());
+            
+            
+            if(date==DateTime.Now.Day && month==DateTime.Now.Month)
+            {
+                Console.WriteLine("Todat it's Your Birthday");
+                Console.WriteLine("Happy Birthday!!!");
+            }
+            else
+            {
+                Console.WriteLine("try again!!!");
+            }
+           
+        }
+    }
+}
