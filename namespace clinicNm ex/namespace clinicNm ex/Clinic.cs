4fdf575092ef9a0ea7183fb539f9c2c9ec39309f
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:51:55 +0530

    Add files via upload

---
+++ namespace clinicNm ex/namespace clinicNm ex/Clinic.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace namespace_clinicNm_ex
+{  
+    internal class  Doctor
+    {
+        private int doctorid;
+        private string doctorname;
+        private string doctoraddress;
+        private string doctoremailid;
+        private int doctoremobilenumber;
+        public void addDoctor(int di,string dn,string da,string de,int mn)
+        {
+            this.doctorid = di;
+            this.doctorname= dn;
+            this.doctoraddress= da;
+            this.doctoremailid= de;
+            this.doctoremobilenumber = mn;
+        }
+        public void dispDoctor()
+        {
+            Console.WriteLine("Doctor id:" + this.doctorid);
+            Console.WriteLine("Doctor name:" + this.doctorname);
+            Console.WriteLine("Doctor address:" + this.doctoraddress);
+            Console.WriteLine("Doctor email id:" + this.doctoremailid);
+            Console.WriteLine("Doctor mobile number:" + this.doctoremobilenumber);
+        }
+    }
+    internal class patient
+    {
+        private int patientid;
+        private string patientname;
+        private string patientaddress;
+        private string patientemailid;
+        private int patientmobilenumber;
+        public void addPatient(int pi,string pn,string pa,string ei,int pm)
+        {
+            this.patientid = pi;
+            this.patientname = pn;
+            this.patientaddress = pa;
+            this.patientemailid = ei;
+            this.patientmobilenumber = pm;
+        }
+        public void display()
+        {
+            Console.WriteLine("patient id:"+this.patientid);
+            Console.WriteLine("patient name:"+this.patientname);
+            Console.WriteLine("patient address:"+this.patientaddress);
+            Console.WriteLine("patient email id:"+this.patientemailid);
+            Console.WriteLine("patient mobile number:"+this.patientmobilenumber);
+        }
+    }
+}
---
+++ namespace clinicNm ex/namespace clinicNm ex/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using namespace_clinicNm_ex;
+
+namespace namespace_clinicNm_ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Doctor d=new Doctor();
+            d.addDoctor(1, "priya", "pune", "priya@123gmail.com", 1234567890);
+            d.dispDoctor();
+
+            patient p = new patient();
+            p.addPatient(2, "chakuli", "shirgaon", "chakuli@123gmail.com", 1999999999);
+            p.display();
+        }
+    }
+}
