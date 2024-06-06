f93a733cf7bf0f84c9995e65446733eabc9a1b30
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:29:46 +0530

    Add files via upload

---
+++ delegate ananymous method ex/delegate ananymous method ex/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegate_ananymous_method_ex
+{
+    delegate int  gdel(int a, int b);
+    delegate void edel(int a);
+    delegate bool check(int a);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            gdel g = delegate (int a, int b)
+            {
+                if (a > b)
+                {
+                    return a;
+                }
+                else
+                {
+                    return b;
+                }
+            };
+            int r = g(5, 9);
+            Console.WriteLine("grater no is:" + r);
+
+            edel e = delegate (int a)
+            {
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine(" number is even:"+a);
+                }
+                else
+                {
+                    Console.WriteLine("number is odd:"+a);
+                }
+
+            };
+            e(45);
+
+            check c = delegate (int a)
+            {
+                return (a > 9 && a < 100);
+            };
+            bool ch = c(50);
+            Console.WriteLine("number is 2 digit:"+ch);
+        }
+    }
+}
