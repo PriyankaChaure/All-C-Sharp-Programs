a42f0e59e2f92f69e120b0d9fbd9e5a340097e63
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:42:14 +0530

    Add files via upload

---
+++ generics_multipletypesymbol/generics_multipletypesymbol/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics_multipletypesymbol
+{
+    class sample<A,B,C,D>
+    {
+        public void display(A a,B b,C c,D d)
+        {
+            Console.WriteLine("A: " + a);
+            Console.WriteLine("B: " + b);
+            Console.WriteLine("C: " + c);
+            Console.WriteLine("D: " + d);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sample<string,int,double,bool>s=new sample<string,int,double,bool>();
+            s.display("priyanka", 123, 345.987, true);
+        }
+    }
+}
