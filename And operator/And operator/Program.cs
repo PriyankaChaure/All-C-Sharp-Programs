dfab713a9eb13a9096ccae9f890929e91850528f
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:22:29 +0530

    Add files via upload

---
+++ And operator/And operator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace And_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age; String gender;
+            Console.WriteLine("enter age");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter gender");
+            gender = Console.ReadLine();
+            if(age>18 && gender=="female")
+            {
+                Console.WriteLine("qualified for job");
+            }
+            else
+                Console.WriteLine("not qualified for job");
+        }
+    }
+}
