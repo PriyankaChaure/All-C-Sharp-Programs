5b2609b865460a3ba3eaa83236407e651fa17601
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:41:05 +0530

    Add files via upload

---
+++ fibonacci/fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n0 = 0, n1 = 1;
+            int n2;
+            Console.WriteLine(n0);
+            Console.WriteLine(n1);
+            for (int i = 0; i < 15; i++)
+            {
+                n2 = n0 + n1;
+                Console.WriteLine(n2);
+                n0 = n1;
+                n1 = n2;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
