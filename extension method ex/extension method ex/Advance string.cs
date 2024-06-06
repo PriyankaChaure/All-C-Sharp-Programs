294cfd2168de9f7c4622478787c0d219503aadaa
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:39:22 +0530

    Add files via upload

---
+++ extension method ex/extension method ex/Advance string.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extension_method_ex
+{
+
+    public static class Advance_string
+    {
+        public static void sayhello(this string obj)
+        {
+            Console.WriteLine("Hello  "+obj);
+        }
+            
+    }
+}
---
+++ extension method ex/extension method ex/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extension_method_ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "priyanka";
+            s.sayhello();
+            string s1 = "prachi";
+            s1.sayhello();
+        }
+    }
+}
