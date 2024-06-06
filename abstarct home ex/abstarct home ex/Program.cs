50a5c9397b873cea9c5fd1f74b650af5b92002f3
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:19:59 +0530

    Add files via upload

---
+++ abstarct home ex/abstarct home ex/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstarct_home_ex
+{  
+    abstract class  shape
+    {
+        public abstract void calculatearea(int d, int l);
+    }
+    class circle : shape
+    {
+        public override void calculatearea( int d, int l)
+        {
+            Console.WriteLine("area of circle is:" + (3.14 * d * d));
+        }
+    }
+    class rectangle :shape
+    {
+        public override void calculatearea(int d,int l)
+        {
+            Console.WriteLine("area of rectangle is:" + (d * l));
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            circle c = new circle();
+            c.calculatearea(3, 0);
+
+            rectangle r = new rectangle();
+            r.calculatearea(5, 4);
+        }
+    }
+}
