3f40a3e60cc934b2a81692da34d6af51991a7db5
Priyanka Chaure <69346609+PriyankaChaure@users.noreply.github.com>
2024-06-06 14:59:02 +0530

    Add files via upload

---
+++ rectangle pattern/rectangle pattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rectangle_pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=4, m=5;
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=m;j++)
+                {
+                    if (i == 1 || j == 1 || i == n || j == m)
+                    {
+                        Console.Write("*");
+                    }
+                    else 
+                    { 
+                        Console.WriteLine("  ");
+                    }
+                }
+                //Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
